503207981d6b9e427a1b722e6ebaea5be3d666aa
yijia2413 <yijia2413@gmail.com>
2013-11-19 04:18:21 +0800

    add pinglun
    Former-commit-id: 1620acda89606304064b7c3377f6aea729bbc3fe

--- js/post.js
+++ js/post.js
@@ -24,14 +24,14 @@ $(document).ready(function(){
     $('pre').addClass('prettyprint linenums'); //添加Google code Hight需要的class
 
     //***********************
-    //**评论的代码也删掉哦***
+    //**评论的代码***
     window.disqus_shortname = 'yijia2413'; // required: replace example with your forum shortname
     $('#disqus_container .comment').on('click',function(){
         $(this).html('加载中...');
         var that = this;
         $.getScript('http://' + disqus_shortname + '.disqus.com/embed.js',function(){$(that).remove()});
     });
-    //**评论的代码也删掉哦***
+    //**评论的代码***
     //***********************
 
     $('.entry a').each(function(index,element){
